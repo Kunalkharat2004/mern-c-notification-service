ecdcc9e3153a13fa5e5204b77441624698f3183a
Kunal <kunalkharat2004@gmail.com>
2025-07-23 19:19:34 +0530

    Add Dockerfile for production setup and enhance Kafka configuration with SSL support for production environment

--- src/config/kafka.ts
+++ src/config/kafka.ts
@@ -1,4 +1,4 @@
-import { Consumer, EachMessagePayload, Kafka } from "kafkajs";
+import { Consumer, EachMessagePayload, Kafka, KafkaConfig } from "kafkajs";
 import config from "config";
 import { MessageBroker } from "../types/broker";
 import { createNotification } from "../factories/notification-factory";
@@ -9,7 +9,24 @@ export class KafkaBroker implements MessageBroker {
   private consumer: Consumer;
 
   constructor(clientId: string, brokers: string[]) {
-    const kafka = new Kafka({ clientId, brokers });
+      let kafkaConfig: KafkaConfig = {
+        clientId,
+        brokers,
+      };
+
+      if (process.env.NODE_ENV === "production") {
+        kafkaConfig = {
+          ...kafkaConfig,
+          ssl: true, // Enable SSL for production
+          connectionTimeout: 45000, // 45 seconds
+          sasl: {
+            mechanism: "plain",
+            username: config.get("kafka.sasl.username"),
+            password: config.get("kafka.sasl.password"),
+          },
+        };
+      }
+    const kafka = new Kafka(kafkaConfig);
 
     this.consumer = kafka.consumer({ groupId: clientId });
   }
